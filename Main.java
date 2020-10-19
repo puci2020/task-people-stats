7e9fc5282145fdb37200abac372126a23fe280b3
puci2020 <adam.pucicki97@gmail.com>
2020-10-19 16:35:19 +0200

    zad 4

--- Main.java
+++ Main.java
@@ -10,12 +10,18 @@ class PeopleStats {
 
 class Person {
     private final String firstName;
+    private final String lastName;
 
-    public Person(String firstName) {
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }

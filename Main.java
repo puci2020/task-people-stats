0ea4f497fad38eb8ba0a921f6a849cb7ee816ed8
puci2020 <adam.pucicki97@gmail.com>
2020-10-19 16:40:12 +0200

    zad 7

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ class Main {
         PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
 
         System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
     }
 }
 
@@ -22,6 +23,13 @@ class PeopleStats {
 
     public long count() {
         return people.size();
+    }
+
+    public long countUniqueLastNames() {
+        return people.stream()
+                .map(Person::getLastName)
+                .distinct()
+                .count();
     }
 }
 
